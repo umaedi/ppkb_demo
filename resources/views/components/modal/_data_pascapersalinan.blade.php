b03acaca12ea0182bebce69b7ffa83a91970341c
= <=>
2023-05-26 11:48:09 +0700

    Umaedi/5/26/23/47

--- resources/views/components/modal/_data_pascapersalinan.blade.php
+++ resources/views/components/modal/_data_pascapersalinan.blade.php
@@ -2,54 +2,196 @@
     <div class="modal-dialog" role="document">
         <div class="modal-content">
             <div class="modal-header">
-                <h5 class="modal-title">Exchange Money</h5>
+                <h5 class="modal-title">DATA SASARAN PASCA PERSALINAN</h5>
             </div>
             <div class="modal-body">
                 <div class="action-sheet-content">
                     <form>
-                        <div class="row">
-                            <div class="col-6">
-                                <div class="form-group basic">
-                                    <div class="input-wrapper">
-                                        <label class="label" for="currency1">From</label>
-                                        <select class="form-control custom-select" id="currency1">
-                                            <option value="1">EUR</option>
-                                            <option value="2">USD</option>
-                                            <option value="3">AUD</option>
-                                            <option value="4">CAD</option>
-                                        </select>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="col-6">
-                                <div class="form-group basic">
-                                    <div class="input-wrapper">
-                                        <label class="label" for="currency2">To</label>
-                                        <select class="form-control custom-select" id="currency2">
-                                            <option value="1">USD</option>
-                                            <option value="1">EUR</option>
-                                            <option value="2">AUD</option>
-                                            <option value="3">CAD</option>
-                                        </select>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-
                         <div class="form-group basic">
-                            <label class="label">Enter Amount</label>
-                            <div class="input-group mb-2">
-                                <span class="input-group-text" id="basic-addon2">$</span>
-                                <input type="text" class="form-control" placeholder="Enter an amount"
-                                    value="100">
+                            <div class="input-wrapper">
+                                <input type="text" class="form-control" id="nik" placeholder="NIK" name="nik_catin_pria">
+                                <i class="clear-input">
+                                    <ion-icon name="close-circle"></ion-icon>
+                                </i>
                             </div>
                         </div>
-
-
-
+                        <div class="form-group basic">
+                            <div class="input-wrapper">
+                                <input type="text" class="form-control" id="ttl" placeholder="Nama" name="alamat_catin_pria">
+                                <i class="clear-input">
+                                    <ion-icon name="close-circle"></ion-icon>
+                                </i>
+                            </div>
+                        </div>
+                        <div class="form-group basic">
+                            <div class="input-wrapper">
+                                <label for="">Tgl Lahir</label>
+                                <input type="date" class="form-control" id="ttl" name="no_tlp_catin_pria">
+                                <i class="clear-input">
+                                    <ion-icon name="close-circle"></ion-icon>
+                                </i>
+                            </div>
+                        </div>
+                        <div class="form-group basic">
+                            <div class="input-wrapper">
+                                <textarea id="textarea4" rows="2" class="form-control"
+                                    placeholder="Alamat"></textarea>
+                                <i class="clear-input">
+                                    <ion-icon name="close-circle"></ion-icon>
+                                </i>
+                            </div>
+                        </div>
+                        <div class="form-group basic">
+                            <div class="input-wrapper">
+                                <input type="text" class="form-control" id="ttl" placeholder="Alamat" name="no_tlp_catin_pria">
+                                <i class="clear-input">
+                                    <ion-icon name="close-circle"></ion-icon>
+                                </i>
+                            </div>
+                        </div>
+                        <h5 class="modal-title">RIWAYAT PERSALINAN</h5>
+                        <div class="form-group basic">
+                            <div class="input-wrapper">
+                                <label for="">Tgl Melahirkan</label>
+                                <input type="date" class="form-control" id="ttl" name="no_tlp_catin_pria">
+                                <i class="clear-input">
+                                    <ion-icon name="close-circle"></ion-icon>
+                                </i>
+                            </div>
+                        </div>
+                        <div class="form-group basic">
+                            <div class="input-wrapper">
+                                <label for="">Tgl Kunjungan</label>
+                                <input type="date" class="form-control" id="ttl" name="no_tlp_catin_pria">
+                                <i class="clear-input">
+                                    <ion-icon name="close-circle"></ion-icon>
+                                </i>
+                            </div>
+                        </div>
+                        <div class="form-group basic">
+                            <div class="input-wrapper">
+                                <select class="form-control custom-select" id="currency2">
+                                    <option value="1">--Tempat Persalinan--</option>
+                                    <option value="1">EUR</option>
+                                    <option value="2">AUD</option>
+                                    <option value="3">CAD</option>
+                                </select>
+                            </div>
+                        </div>
+                        <div class="form-group basic">
+                            <div class="input-wrapper">
+                                <select class="form-control custom-select" id="currency2">
+                                    <option value="1">--Penolong Persalinan--</option>
+                                    <option value="1">EUR</option>
+                                    <option value="2">AUD</option>
+                                    <option value="3">CAD</option>
+                                </select>
+                            </div>
+                        </div>
+                        <div class="form-group basic">
+                            <div class="input-wrapper">
+                                <select class="form-control custom-select" id="currency2">
+                                    <option value="1">--Cara Persalinan--</option>
+                                    <option value="1">EUR</option>
+                                    <option value="2">AUD</option>
+                                    <option value="3">CAD</option>
+                                </select>
+                            </div>
+                        </div>
+                        <div class="form-group basic">
+                            <div class="input-wrapper">
+                                <select class="form-control custom-select" id="currency2">
+                                    <option value="1">--Apakah Ibu Mengalami Komplikasi Pada Masa Nifas--</option>
+                                    <option value="1">EUR</option>
+                                    <option value="2">AUD</option>
+                                    <option value="3">CAD</option>
+                                </select>
+                            </div>
+                        </div>
+                        <div class="form-group basic">
+                            <div class="input-wrapper">
+                                <select class="form-control custom-select" id="currency2">
+                                    <option value="1">--Keadaan Bayi--</option>
+                                    <option value="1">EUR</option>
+                                    <option value="2">AUD</option>
+                                    <option value="3">CAD</option>
+                                </select>
+                            </div>
+                        </div>
+                        <h5 class="modal-title">Pengguna KB Pasca Persalinan</h5>
+                        <div class="form-group basic">
+                            <div class="input-wrapper">
+                                <select class="form-control custom-select" id="currency2">
+                                    <option value="1">--Pengguna KB Pasca Persalinan--</option>
+                                    <option value="1">EUR</option>
+                                    <option value="2">AUD</option>
+                                    <option value="3">CAD</option>
+                                </select>
+                            </div>
+                        </div>
+                        <h5 class="modal-title">HASIL PEMERIKSAAN IBU HAMIL</h5>
+                        <div class="section full mt-2 mb-2">
+                            <div class="wide-block p-0">
+                                <div class="section-title">Memberikan penyuluhan/KIE</div>
+                                <div class="input-list">
+                                    <div class="form-check">
+                                        <input class="form-check-input" type="radio" name="radioList" id="radioList3">
+                                        <label class="form-check-label" for="radioList3">Ya</label>
+                                    </div>
+                                    <div class="form-check">
+                                        <input class="form-check-input" type="radio" name="radioList" id="radioList3.1">
+                                        <label class="form-check-label" for="radioList3.1">Tidak</label>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="wide-block p-0">
+                                <div class="section-title">Memfasilitasi Pelayanan Rujukan</div>
+                                <div class="input-list">
+                                    <div class="form-check">
+                                        <input class="form-check-input" type="radio" name="radioList" id="radioList3">
+                                        <label class="form-check-label" for="radioList3">Ya</label>
+                                    </div>
+                                    <div class="form-check">
+                                        <input class="form-check-input" type="radio" name="radioList" id="radioList3.1">
+                                        <label class="form-check-label" for="radioList3.1">Tidak</label>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="wide-block p-0">
+                                <div class="section-title">Memfasilitasi Bantuan Sosial</div>
+                                <div class="input-list">
+                                    <div class="form-check">
+                                        <input class="form-check-input" type="radio" name="radioList" id="radioList3">
+                                        <label class="form-check-label" for="radioList3">Ya</label>
+                                    </div>
+                                    <div class="form-check">
+                                        <input class="form-check-input" type="radio" name="radioList" id="radioList3.1">
+                                        <label class="form-check-label" for="radioList3.1">Tidak</label>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="form-group basic">
+                                <div class="input-wrapper">
+                                    <label for="">Tgl Kunjungan Berikut</label>
+                                    <input type="date" class="form-control" id="ttl" name="no_tlp_catin_pria">
+                                    <i class="clear-input">
+                                        <ion-icon name="close-circle"></ion-icon>
+                                    </i>
+                                </div>
+                            </div>
+                            <div class="form-group basic">
+                                <div class="input-wrapper">
+                                    <textarea id="textarea4" rows="2" class="form-control"
+                                        placeholder="Catatan TPK"></textarea>
+                                    <i class="clear-input">
+                                        <ion-icon name="close-circle"></ion-icon>
+                                    </i>
+                                </div>
+                            </div>
+                        </div>
                         <div class="form-group basic">
                             <button type="button" class="btn btn-primary btn-block btn-lg"
-                                data-bs-dismiss="modal">Exchange</button>
+                                data-bs-dismiss="modal">Simpan</button>
                         </div>
                     </form>
                 </div>
