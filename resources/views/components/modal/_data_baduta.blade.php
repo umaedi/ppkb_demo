22e179805d517958d6fed46e0089bf68b014efe3
= <=>
2023-05-27 19:33:27 +0700

    Umaedi/5/27/23

--- resources/views/components/modal/_data_baduta.blade.php
+++ resources/views/components/modal/_data_baduta.blade.php
@@ -2,54 +2,180 @@
     <div class="modal-dialog" role="document">
         <div class="modal-content">
             <div class="modal-header">
-                <h5 class="modal-title">Exchange Money</h5>
+                <h5 class="modal-title">DATA SASARAN BADUTA</h5>
             </div>
             <div class="modal-body">
                 <div class="action-sheet-content">
                     <form>
-                        <div class="row">
-                            <div class="col-6">
-                                <div class="form-group basic">
-                                    <div class="input-wrapper">
-                                        <label class="label" for="currency1">From</label>
-                                        <select class="form-control custom-select" id="currency1">
-                                            <option value="1">EUR</option>
-                                            <option value="2">USD</option>
-                                            <option value="3">AUD</option>
-                                            <option value="4">CAD</option>
-                                        </select>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="col-6">
-                                <div class="form-group basic">
-                                    <div class="input-wrapper">
-                                        <label class="label" for="currency2">To</label>
-                                        <select class="form-control custom-select" id="currency2">
-                                            <option value="1">USD</option>
-                                            <option value="1">EUR</option>
-                                            <option value="2">AUD</option>
-                                            <option value="3">CAD</option>
-                                        </select>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-
                         <div class="form-group basic">
-                            <label class="label">Enter Amount</label>
-                            <div class="input-group mb-2">
-                                <span class="input-group-text" id="basic-addon2">$</span>
-                                <input type="text" class="form-control" placeholder="Enter an amount"
-                                    value="100">
+                            <div class="input-wrapper">
+                                <label for="date">Tgl Kunjungan</label>
+                                <input type="date" name="tgl_kunjungan" id="date" class="form-control">
+                                <i class="clear-input">
+                                    <ion-icon name="close-circle"></ion-icon>
+                                </i>
+                            </div>
+                            <div class="input-wrapper">
+                                <input type="number" name="#" id="nik" class="form-control" placeholder="NIK">
+                                <i class="clear-input">
+                                    <ion-icon name="close-circle"></ion-icon>
+                                </i>
+                            </div>
+                            <div class="input-wrapper">
+                                <input type="text" name="#" class="form-control" placeholder="Nama">
+                                <i class="clear-input">
+                                    <ion-icon name="close-circle"></ion-icon>
+                                </i>
+                            </div>
+                            <div class="input-wrapper">
+                                <input type="text" name="#" class="form-control" placeholder="Alamat">
+                                <i class="clear-input">
+                                    <ion-icon name="close-circle"></ion-icon>
+                                </i>
+                            </div>
+                            <div class="input-wrapper">
+                                <input type="date" name="#" class="form-control" placeholder="Tgl Lahir Ibu">
+                                <i class="clear-input">
+                                    <ion-icon name="close-circle"></ion-icon>
+                                </i>
+                            </div>
+                            <div class="input-wrapper">
+                                <input type="number" name="#" class="form-control" placeholder="No HP/WhatsApp">
+                                <i class="clear-input">
+                                    <ion-icon name="close-circle"></ion-icon>
+                                </i>
+                            </div>
+                            <div class="input-wrapper">
+                                <input type="number" name="#" class="form-control" placeholder="No HP/WhatsApp">
+                                <i class="clear-input">
+                                    <ion-icon name="close-circle"></ion-icon>
+                                </i>
+                            </div>
+                            <div class="form-group basic">
+                                <div class="input-wrapper">
+                                    <select class="form-control custom-select" id="currency1">
+                                        <option value="1">--Penggunaan Kontrasepsi Saat Ini--</option>
+                                        <option value="2">Ya</option>
+                                        <option value="3">Tidak</option>
+                                    </select>
+                                </div>
+                            </div>
+                            <div class="form-group basic">
+                                <div class="input-wrapper">
+                                    <select class="form-control custom-select" id="currency1">
+                                        <option value="1">--Buang Air Besar Ditempat Yang Layak--</option>
+                                        <option value="2">Ya</option>
+                                        <option value="3">Tidak</option>
+                                    </select>
+                                </div>
+                            </div>
+                            <div class="input-wrapper mt-3">
+                                <h5 class="modal-title">DATA BADUTA</h5>
+                                <input type="number" name="#" id="nik" class="form-control" placeholder="NIK Anak">
+                                <i class="clear-input">
+                                    <ion-icon name="close-circle"></ion-icon>
+                                </i>
+                            </div>
+                            <div class="input-wrapper mt-3">
+                                <input type="text" name="#" id="nik" class="form-control" placeholder="Nama Baduta">
+                                <i class="clear-input">
+                                    <ion-icon name="close-circle"></ion-icon>
+                                </i>
+                            </div>
+                            <div class="input-wrapper mt-3">
+                                <label for="">Tgl Lahir Baduta</label>
+                                <input type="date" name="#" id="nik" class="form-control">
+                                <i class="clear-input">
+                                    <ion-icon name="close-circle"></ion-icon>
+                                </i>
+                            </div>
+                            <div class="form-group basic">
+                                <div class="input-wrapper">
+                                    <select class="form-control custom-select" id="currency1">
+                                        <option value="1">--Jenis Kelamin Baduta--</option>
+                                        <option value="2">Laki-Laki</option>
+                                        <option value="3">Perempuan</option>
+                                    </select>
+                                </div>
+                            </div>
+                            <div class="input-wrapper mt-3">
+                                <input type="number" name="#" class="form-control" placeholder="Urutan Anak Ke">
+                                <i class="clear-input">
+                                    <ion-icon name="close-circle"></ion-icon>
+                                </i>
+                            </div>
+                            <div class="input-wrapper mt-3">
+                                <input type="number" name="#" class="form-control" placeholder="Berat Badan Lahir (kg)">
+                                <i class="clear-input">
+                                    <ion-icon name="close-circle"></ion-icon>
+                                </i>
+                            </div>
+                            <div class="input-wrapper mt-3">
+                                <input type="number" name="#" class="form-control" placeholder="Panjang Badan Lahir (cm)">
+                                <i class="clear-input">
+                                    <ion-icon name="close-circle"></ion-icon>
+                                </i>
+                            </div>
+                            <h5 class="modal-title">DATA DUKUNG LAINNYA</h5>
+                            <div class="form-group basic">
+                                <div class="input-wrapper">
+                                    <select class="form-control custom-select" id="currency1">
+                                        <option value="1">--Kehadiran Pada Posyandu--</option>
+                                        <option value="2">Ya</option>
+                                        <option value="3">Tidak</option>
+                                    </select>
+                                </div>
+                            </div>
+                            <div class="form-group basic">
+                                <div class="input-wrapper">
+                                    <select class="form-control custom-select" id="currency1">
+                                        <option value="1">--Memberikan Penyuluhan/KIE--</option>
+                                        <option value="2">Ya</option>
+                                        <option value="3">Tidak</option>
+                                    </select>
+                                </div>
+                            </div>
+                            <div class="form-group basic">
+                                <div class="input-wrapper">
+                                    <select class="form-control custom-select" id="currency1">
+                                        <option value="1">--Pemberian Fasilitas Pelayanan Rujukan--</option>
+                                        <option value="2">Ya, Sedang Dalam Proses</option>
+                                        <option value="3">Ya, Sudah Mendapatkan Pelayanan Rujukan</option>
+                                        <option value="3">Tidak</option>
+                                    </select>
+                                </div>
+                            </div>
+                            <div class="form-group basic">
+                                <div class="input-wrapper">
+                                    <select class="form-control custom-select" id="currency1">
+                                        <option value="1">--Memfasilitasi Bantuan Sosial--</option>
+                                        <option value="2">Ya, Sedang Dalam Proses</option>
+                                        <option value="3">Ya, Sudah Mendapatkan Bantuan Sosial</option>
+                                        <option value="3">Tidak, Karen Tidak Memenuhi Syarat</option>
+                                        <option value="3">Tidak, Karena Sudah Mendapatkan Bantuan Sosial</option>
+                                    </select>
+                                </div>
+                            </div>
+                            <div class="input-wrapper">
+                                <label for="date">Tgl Kunjungan Berikutnya</label>
+                                <input type="date" name="tgl_kunjungan" id="date" class="form-control">
+                                <i class="clear-input">
+                                    <ion-icon name="close-circle"></ion-icon>
+                                </i>
+                            </div>
+                            <div class="form-group basic">
+                                <div class="input-wrapper">
+                                    <textarea id="textarea4" rows="2" class="form-control"
+                                        placeholder="Catatan TPK"></textarea>
+                                    <i class="clear-input">
+                                        <ion-icon name="close-circle"></ion-icon>
+                                    </i>
+                                </div>
                             </div>
                         </div>
-
-
-
                         <div class="form-group basic">
                             <button type="button" class="btn btn-primary btn-block btn-lg"
-                                data-bs-dismiss="modal">Exchange</button>
+                                data-bs-dismiss="modal">Simpan</button>
                         </div>
                     </form>
                 </div>
